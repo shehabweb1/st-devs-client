9d230621e983e2b8af6e072a66a06a4fea0d9ff5
MD Shehab Hossen <shehab.web1@gmail.com>
2023-11-06 02:24:45 +0600

    About and Contact Page Done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,110 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
-    return (
-        <div>
-            This is about page
-        </div>
-    );
+	return (
+		<div>
+			<div
+				className="hero min-h-fit"
+				style={{
+					backgroundImage:
+						"url(https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg)",
+				}}
+			>
+				<div className="bg-[#000000b3] w-full h-full"></div>
+				<div className="hero-content text-center py-36 text-white">
+					<div className="max-w-lg">
+						<h1 className="text-5xl font-bold">ABOUT US</h1>
+						<p className="py-6 text-2xl font-semibold">
+							Welcome to ST Dev&#39;s
+						</p>
+					</div>
+				</div>
+			</div>
+			<div className="container mx-auto py-10">
+				<div className="mb-5">
+					<h3 className="text-xl font-semibold mb-3">Who We Are</h3>
+					<p className="mb-3">
+						We are a passionate team of web developers and enthusiasts,
+						dedicated to sharing our knowledge and insights with the world. Our
+						journey in the world of web development began with a simple idea –
+						to make the web a better place through creativity, innovation, and
+						the power of coding.
+					</p>
+				</div>
+				<div className="py-5">
+					<h2 className="text-xl font-semibold mb-3">Our Mission</h2>
+					<p>
+						Our mission is to empower aspiring web developers, designers, and
+						tech enthusiasts with the information, resources, and inspiration
+						they need to create amazing web experiences. We believe that the
+						internet is an ever-evolving canvas, and our goal is to help you
+						paint your vision with the digital brush.
+					</p>
+				</div>
+				<div className="py-5">
+					<h2 className="text-xl font-semibold mb-3">What We Offer</h2>
+					<ul className="list-disc list-inside">
+						<li>
+							<span className="font-bold">Informative Blogs: </span>
+							Our blog posts cover a wide range of web development topics, from
+							front-end design to back-end coding, SEO strategies, and the
+							latest web development trends.
+						</li>
+						<li>
+							<span className="font-bold">Tutorials: </span>
+							We provide step-by-step tutorials to guide you through building
+							your own websites and web applications.
+						</li>
+						<li>
+							<span className="font-bold">Community: </span>
+							We foster a welcoming community where developers can share their
+							ideas, ask questions, and learn from each other.
+						</li>
+					</ul>
+				</div>
+				<div className="py-5">
+					<h2 className="text-xl font-semibold mb-3">Why Choose Us</h2>
+					<ul className="list-disc list-inside">
+						<li>
+							<span className="font-bold">Expertise: </span>
+							Our team consists of experienced web developers who have their
+							fingers on the pulse of the industry.
+						</li>
+						<li>
+							<span className="font-bold">Passion: </span>
+							We are not just web developers; we are web development
+							enthusiasts. We love what we do, and we want to share that passion
+							with you.
+						</li>
+						<li>
+							<span className="font-bold">Up-to-Date Information: </span>
+							The web development world evolves rapidly, and we ensure our
+							content is always current and relevant.
+						</li>
+					</ul>
+				</div>
+				<div className="py-5">
+					<h2 className="text-xl font-semibold mb-3">
+						Join Us on This Journey
+					</h2>
+					<p>
+						We believe in the power of the web to connect people, ideas, and
+						possibilities. Whether you&#39;re a beginner or an experienced
+						developer, we invite you to join us on this exciting journey of web
+						development.
+					</p>
+					<p>
+						Thank you for visiting ST Dev&#39;s, and we look forward to being
+						your companion in the world of web development.
+					</p>
+					<p>
+						If you have any questions, suggestions, or feedback, please feel
+						free to <Link to="/contact" className="text-blue-500">contact us</Link>.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,111 @@
+import { HiOfficeBuilding, HiPhone } from "react-icons/hi";
+import { MdLocalPostOffice } from "react-icons/md";
+import { BsFillSendFill } from "react-icons/bs";
+import Swal from "sweetalert2";
+
 const Contact = () => {
-    return (
-        <div>
-            This is contact Page
-        </div>
-    );
+	const handleContactForm = (e) => {
+		e.preventDefault();
+		const form = e.target;
+		Swal.fire(
+			"Thank You!",
+			"Your message has been sent successfully!",
+			"success"
+		);
+		form.name.value = "";
+		form.email.value = "";
+		form.message.value = "";
+	};
+	return (
+		<div>
+			<div
+				className="hero min-h-fit"
+				style={{
+					backgroundImage:
+						"url(https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg)",
+				}}
+			>
+				<div className="bg-[#000000b3] w-full h-full"></div>
+				<div className="hero-content text-center py-36 text-white">
+					<div className="max-w-lg">
+						<h1 className="text-5xl font-bold">CONTACT US</h1>
+						<p className="py-6 text-2xl font-semibold">
+							Welcome to ST Dev&#39;s
+						</p>
+					</div>
+				</div>
+			</div>
+			<div className="container mx-auto my-10">
+				<div className="grid grid-cols-1 lg:grid-cols-2 mx-auto gap-5">
+					<div className="bg-base-300 py-5 rounded-2xl">
+						<h3 className="text-2xl text-center font-semibold mb-3">
+							Our Contact Details
+						</h3>
+						<div className="ml-6">
+							<p className="flex items-center justify-center text-xl mt-10">
+								<HiOfficeBuilding className="text-2xl mr-1" /> Mohammadpur,
+								Dhaka-1207
+							</p>
+							<p className="flex items-center justify-center text-xl my-5">
+								<MdLocalPostOffice className="text-2xl mr-1" />{" "}
+								contact@stdevs.com
+							</p>
+							<p className="flex items-center justify-center text-xl">
+								<HiPhone className="text-2xl mr-1" /> +8801608451204
+							</p>
+						</div>
+					</div>
+					<div className="py-5">
+						<h3 className="text-2xl text-center font-semibold mb-3">
+							Our Contact Form
+						</h3>
+						<form onSubmit={handleContactForm} className="w-3/4 mx-auto">
+							<fieldset className="form-control">
+								<label className="label">
+									<span className="label-text">Enter your Full Name</span>
+								</label>
+								<input
+									type="text"
+									name="name"
+									placeholder="Enter your Full Name"
+									className="input input-bordered w-full pr-16"
+									required
+								/>
+							</fieldset>
+							<fieldset className="form-control">
+								<label className="label">
+									<span className="label-text">Enter your email address</span>
+								</label>
+								<input
+									type="email"
+									name="email"
+									placeholder="Enter your email address"
+									className="input input-bordered w-full pr-16"
+									required
+								/>
+							</fieldset>
+							<fieldset className="form-control">
+								<label className="label">
+									<span className="label-text">Write a Message</span>
+								</label>
+								<textarea
+									className="textarea textarea-bordered"
+									placeholder="Write a Message"
+									name="message"
+								></textarea>
+							</fieldset>
+							<button
+								type="submit"
+								className="btn bg-blue-600 hover:bg-blue-700 text-white mt-3"
+							>
+								<BsFillSendFill /> Send
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
