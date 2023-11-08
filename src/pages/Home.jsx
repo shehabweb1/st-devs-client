58e71cfa50864e1e8837d88c1f3cf7ab08696434
MD Shehab Hossen <shehab.web1@gmail.com>
2023-11-08 21:39:04 +0600

    Framer Motion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,29 @@ import NewsLetter from "../components/NewsLetter";
 import RecentBlog from "../components/RecentBlog";
 import WhyChooseUs from "../components/WhyChooseUs";
 import OurPartners from "./../components/OurPartners";
+import { motion, useScroll } from "framer-motion";
 
 const Home = () => {
-	
+	const { scrollYProgress } = useScroll();
+
 	return (
 		<>
-			<Banner />
-			<RecentBlog />
-			<WhyChooseUs />
-			<NewsLetter />
-			<OurPartners />
+			<motion.path
+				d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+				style={{ pathLength: scrollYProgress }}
+			>
+				<Banner />
+				<motion.div
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					viewport={{ once: true }}
+				>
+					<RecentBlog />
+				</motion.div>
+				<WhyChooseUs />
+				<NewsLetter />
+				<OurPartners />
+			</motion.path>
 		</>
 	);
 };
