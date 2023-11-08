0fe92a8a3431fab0fea1c70acd96184da3a09faf
MD Shehab Hossen <shehab.web1@gmail.com>
2023-11-08 21:45:57 +0600

    Wishlist Page Updated

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -47,32 +47,40 @@ const Wishlist = () => {
 	return (
 		<>
 			<div className="py-10">
-				{wishlistBlog.map((data) => (
-					<div
-						className="mb-4 grid grid-cols-4 gap-2 border shadow-lg md:mx-4"
-						key={data._id}
-					>
-						<img src={data.image} alt={data.title} className="w-auto" />
-						<div className="col-span-2 md:py-3">
-							<h3 className="lg:text-3xl font-semibold mb-2">{data.title}</h3>
-							<p className="badge badge-info lg:my-3">{data.category}</p>
-							<p className="hidden md:block">{data.shortDesc}</p>
-						</div>
-						<div className="my-auto mx-auto">
-							<Link to={`/blogDetails/${data.id}`}>
-								<button className="block mb-3 btn py-1 px-3 text-white bg-blue-600 hover:bg-blue-700">
-									Details
+				{wishlistBlog > 0 ? (
+					wishlistBlog.map((data) => (
+						<div
+							className="mb-4 grid grid-cols-4 gap-2 border shadow-lg md:mx-4"
+							key={data._id}
+						>
+							<img src={data.image} alt={data.title} className="w-auto" />
+							<div className="col-span-2 md:py-3">
+								<h3 className="lg:text-3xl font-semibold mb-2">{data.title}</h3>
+								<p className="badge badge-info lg:my-3">{data.category}</p>
+								<p className="hidden md:block">{data.shortDesc}</p>
+							</div>
+							<div className="my-auto mx-auto">
+								<Link to={`/blogDetails/${data.id}`}>
+									<button className="block mb-3 btn py-1 px-3 text-white bg-blue-600 hover:bg-blue-700">
+										Details
+									</button>
+								</Link>
+								<button
+									onClick={() => handleRemove(data._id)}
+									className="btn py-1 px-3 text-white bg-red-600 hover:bg-red-800"
+								>
+									Remove
 								</button>
-							</Link>
-							<button
-								onClick={() => handleRemove(data._id)}
-								className="btn py-1 px-3 text-white bg-red-600 hover:bg-red-800"
-							>
-								Remove
-							</button>
+							</div>
 						</div>
+					))
+				) : (
+					<div className="py-10">
+						<h2 className="text-xl font-semibold text-center">
+							You haven&#39;t add wishlist{" "}
+						</h2>
 					</div>
-				))}
+				)}
 			</div>
 		</>
 	);
