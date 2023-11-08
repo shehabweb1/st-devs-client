9c6d34e816a61d71f6cdb5ffd777d740e49f2e35
MD Shehab Hossen <shehab.web1@gmail.com>
2023-11-08 16:07:34 +0600

    Custom Hooks Done

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,10 +1,9 @@
 import { useContext } from "react";
-import { AuthContext } from "../providers/AuthProvider";
-
+import { UserProviderContext } from "../authProvider/AuthProvider";
 
 const useAuth = () => {
-    const auth = useContext(AuthContext);
-    return auth;
+	const auth = useContext(UserProviderContext);
+	return auth;
 };
 
 export default useAuth;
