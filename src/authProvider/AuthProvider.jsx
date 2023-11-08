4319d1737c49615f4247ef08ebae473716b6844c
MD Shehab Hossen <shehab.web1@gmail.com>
2023-11-08 19:02:08 +0600

    Authenticetions issue solve

--- src/authProvider/AuthProvider.jsx
+++ src/authProvider/AuthProvider.jsx
@@ -69,7 +69,7 @@ const AuthProvider = ({ children }) => {
 		return () => {
 			unSubscribe();
 		};
-	}, []);
+	}, [user]);
 
 	const authValues = {
 		user,
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -18,9 +18,7 @@ const useAxiosSecure = () => {
 				return res;
 			},
 			(error) => {
-				console.log("error tracked in the interceptor", error.response);
 				if (error.response.status === 401 || error.response.status === 403) {
-					console.log("logout the user");
 					logOut()
 						.then(() => {
 							navigate("/login");
@@ -29,7 +27,7 @@ const useAxiosSecure = () => {
 				}
 			}
 		);
-	}, []);
+	}, [logOut, navigate]);
 
 	return axiosSecure;
 };
